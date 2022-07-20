42b254492c8ea7d0de5dbf83dfa44365422429d2
als904204 <als904204>
2022-07-20 15:46:59 +0900

    댓글 작성저 ㄴ

---
+++ src/main/java/com/toy/fifa/Controller/ReplyController.java
@@ -0,0 +1,13 @@
+package com.toy.fifa.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ReplyController {
+
+
+
+}
