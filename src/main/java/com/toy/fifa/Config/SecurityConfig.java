2ac6c538fc9be73501894cf1a57d2c3a9a067def
als904204 <als904204>
2022-08-01 10:35:58 +0900

    시큐리티 설정파일 로그아웃시 세션 삭제 및 로그인 페이지 디자인 완성

--- src/main/java/com/toy/fifa/Config/SecurityConfig.java
+++ src/main/java/com/toy/fifa/Config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.toy.fifa.Config;
 
 
 import com.toy.fifa.DTO.ResponseMessage;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,14 +24,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
+    // 로그아웃하면 세션삭제
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**")
-                .permitAll();
+                .antMatchers("/**").permitAll()
+            .and().formLogin()
+                .loginPage("/user/join")
+                .defaultSuccessUrl("/")
+                .permitAll()
+            .and().logout().
+                invalidateHttpSession(true) // remove session
+                .logoutSuccessUrl("/")
+                .permitAll()
+            .and().sessionManagement()
+                .maximumSessions(1) // 세션 허용 개수
+                .expiredUrl("/user/join")  // 세션 만료시 이동 페이지
+                .maxSessionsPreventsLogin(true); // 동일한 사용자 로그인 불가능
+
     }
 }
